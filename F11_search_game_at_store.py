4162066a6385c2631b22e0f217a022cb57d1af18
Mutiara Arafah Sihombing <88826274+mutiarfhs@users.noreply.github.com>
2022-04-23 23:09:28 +0700

    Update F11_search_game_at_store.py

--- F11_search_game_at_store.py
+++ F11_search_game_at_store.py
@@ -1,31 +1,35 @@
 from primitif_function import *
 
 
-def search_game_at_store():
-    temp_data = data_constructor("game.csv")
+def search_game_at_store(arrGame):  # input(
+    temp_data = arrGame[0]  # Parsed CSV
+    data_length = arrGame[1]  # Neff (Length parsed csv)
 
-    selection_q = ["ID", "Nama", "Kategori", "Tahun Rilis", "Harga"]
-    selection_list = [input("Masukan " + i + " Game :") for i in selection_q]
+    selection_q = ["ID", "Nama", "Kategori", "Tahun Rilis", "Harga"]  # selection question
+    selection_list = [input("Masukan " + i + " Game :") for i in selection_q]  # selection list
 
-    indx = 0
+    indx = 0  # index
 
-    for q in selection_list:
-        if q == "":
+    # game selection from selection_list
+    for i in range(5):
+        if selection_list[i] == "":
             indx += 1
             pass
         else:
-            game_data = [["", "", "", "", ""]]
-            for game_line in temp_data:
-                if game_line[indx] == q:
-                    game_data += [game_line]
+            game_data = []
+            for j in range(data_length):
+                if temp_data[j][indx] == selection_list[i]:
+                    game_data += [temp_data[j]]
             temp_data = game_data
             indx += 1
 
-    if length(temp_data) == 1:
-        pass
+    # output from filtered game
+    if length(temp_data) == 0:
+        print("Tidak ada game yang cocok")
     else:
         for i in range(1, length(temp_data)):
-            print(temp_data[i])
-
-
-search_game_at_store()
+            for j in range(6):
+                if j != 5:
+                    print(temp_data[i][j], end=" | ")
+                else:
+                    print(temp_data[i][j])
