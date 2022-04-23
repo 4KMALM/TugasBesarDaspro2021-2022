9560f4cc1ac4f529ce171954e0dc3d39f00397c6
Akmal Mahardika N P <89432383+4KMALM@users.noreply.github.com>
2022-04-23 18:53:22 +0700

    Update F06_ubah_stock.py

--- F06_ubah_stock.py
+++ F06_ubah_stock.py
@@ -3,15 +3,23 @@ from primitif_function import *
 
 def ubah_stock(arrGame):
     id_game = input()
-    id_found = search_found(id_game,0,arrGame)
-    if id_found:
+    # temukan id
+    search_found = get_idx(id_game,0,arrGame)
+
+    if id_found != -1:
+        id_found = get_idx(id_game,0,arrGame)
+        # cari nama game dan stock awalnya
         name_game = get_thing_based(id_game,0,arrGame,1)
-        init_stock = get_thing_based(id_game,0,arrGame,5)
+        init_stock = int(get_thing_based(id_game,0,arrGame,5))
+
         stock_in = int(input())
+        
         if init_stock+stock_in < 0 :
             print(f"Stok game {name_game} gagal dikurangi karena stok kurang. Stok sekarang: {init_stock} (< {abs(stock_in)})")
         else:
             fnl_stock = init_stock + stock_in
+            arrGame[0][id_found][5] = fnl_stock
             print(f"Stok game {name_game} berhasil dikurangi. Stok sekarang: {fnl_stock}")
+            return(arrGame)
     else:
         print("Tidak ada game dengan ID tersebut!")
