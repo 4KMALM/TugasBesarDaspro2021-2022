fc3ae2661a7581cecb2e8457acdf13fbd37c7b83
rimasfna <102893447+rimasfna@users.noreply.github.com>
2022-04-23 16:26:01 +0700

    Add files via upload

---
+++ F12_Top Up Saldo.py
@@ -0,0 +1,18 @@
+from primitif_function import *
+
+def topUp(arrUser):
+    usernm = input("Masukan username: ")
+    plusSldo = int(input("Masukan saldo: "))
+    found = search_found(usernm,1,arrUser)
+    print()
+    if found:
+        cekSaldo = get_thing_based(usernm,1,arrUser,5)
+        cekNama = get_thing_based(usernm,1,arrUser,2)
+        totalSaldo = cekSaldo + plusSldo
+        if totalSaldo >= 0 :
+            print(f"Top up berhasil. Saldo {cekNama} bertambah menjadi {totalSaldo}")
+        else:
+            print(f"Masukan tidak valid")
+
+    else:
+        print(f"Username {usernm} tidak ditemukan")
