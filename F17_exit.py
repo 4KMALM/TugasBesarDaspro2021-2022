62337ce83918b08d4a5342ca90e4a32a85355099
rimasfna <102893447+rimasfna@users.noreply.github.com>
2022-04-24 00:16:38 +0700

    Update F17_exit.py

--- F17_exit.py
+++ F17_exit.py
@@ -1,19 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
+from primitif_function import *
 
-# In[7]:
-
-
-def exit():
+def exit(x):
     print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
     pil_exit = input()
     if pil_exit == "Y" or pil_exit == "y" :
-        save()
+        save() # Memanggil fungsi save
     elif pil_exit == "N" or pil_exit == "n":
         print("Program selesai dan tidak menyimpan data pada file.")
     else : #jika input != Y, y, n, atau N
-        exit()
+        exit(x)
     return
 
-exit()
+exit(x)
 
