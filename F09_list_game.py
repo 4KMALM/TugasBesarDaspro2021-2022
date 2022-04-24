d5f5f1463ce6517f84e661290ceb53ad8982e60a
mdrmnd <mdarmansyah2003@gmail.com>
2022-04-24 17:25:52 +0700

    Update F09

--- F09_list_game.py
+++ F09_list_game.py
@@ -1,8 +1,10 @@
 from primitif_function import *
 
 
-def list_game(kepemilikan, game_data, user_id) :
+def list_game(arrKepemilikan, arrGame, user_id) :
     data_game = []
+    kepemilikan = arrKepemilikan[0]
+    game_data = arrGame[0]
     for pemilik in kepemilikan :
         if user_id == pemilik[1] :
             data_game += [pemilik[0]]
@@ -11,7 +13,6 @@ def list_game(kepemilikan, game_data, user_id) :
     else :
         for game in game_data :
             if game[0] in data_game :
-                for i in range(5) :
-                    print(game[i],end="|")
-                print()
-
+                print("| {:<8}| {:<36}| {:<14}| {:<4}| {:<8}".format(game[0],game[1],game[2],game[3],game[4]))
+            else :
+                pass
