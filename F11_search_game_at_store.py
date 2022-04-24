2493811b98ce85e06a9025a9a3cbd868b597a338
Akmal Mahardika N P <89432383+4KMALM@users.noreply.github.com>
2022-04-24 21:03:35 +0700

    search_game_at_store Jadi

--- F11_search_game_at_store.py
+++ F11_search_game_at_store.py
@@ -1,35 +1,37 @@
+from operator import length_hint
 from primitif_function import *
 
+def search_game_at_store(arrGame):
+# Menerima masukan data game dari user lalu mencari data tersebut dalam toko dan menuliskan hasil yang sesuai
+# I.S arrGame terdefinisi
+# F.S. menuliskan ke layar game-game sesuai masukan user
 
-def search_game_at_store(arrGame):  # input(
+# KAMUS LOKAL
+# constant
+    # selection_q = list
+# Variabel
+    # temp_data, selection_list = list
+    # data_lenght = int
+# ALGORITMA
     temp_data = arrGame[0]  # Parsed CSV
     data_length = arrGame[1]  # Neff (Length parsed csv)
 
-    selection_q = ["ID", "Nama", "Kategori", "Tahun Rilis", "Harga"]  # selection question
-    selection_list = [input("Masukan " + i + " Game :") for i in selection_q]  # selection list
-
-    indx = 0  # index
-
-    # game selection from selection_list
-    for i in range(5):
-        if selection_list[i] == "":
-            indx += 1
-            pass
-        else:
-            game_data = []
-            for j in range(data_length):
-                if temp_data[j][indx] == selection_list[i]:
-                    game_data += [temp_data[j]]
-            temp_data = game_data
-            indx += 1
-
-    # output from filtered game
-    if length(temp_data) == 0:
-        print("Tidak ada game yang cocok")
+    if arrGame[1] == 0: # Penanganan kasus
+        print("Mohon maaf daftar game sedang tidak tersedia")
     else:
-        for i in range(1, length(temp_data)):
-            for j in range(6):
-                if j != 5:
-                    print(temp_data[i][j], end=" | ")
-                else:
-                    print(temp_data[i][j])
+        selection_q = ["ID", "Nama", "Kategori", "Tahun Rilis", "Harga"]  # selection question
+        selection_list = [input("Masukan " + i + " Game : ") for i in selection_q]  # selection list
+
+        if data_length-1 == 0:
+            print("Tidak ada game yang cocok")
+        else:
+            print("{:<4}|{:^9}|{:^36}|{:^16}|{:^13}|{:^11}|{:^6}|".format("No","ID Game","Nama Game","Kategori","Tahun Rilis","Harga","stok"))
+            print("-"*102)
+            j = 0
+
+            for i in range(1,data_length+1):
+                if (selection_list[0] == temp_data[i][0] or selection_list[0] == "") and (selection_list[1] == temp_data[i][1] or selection_list[1] == "") and (selection_list[2] == temp_data[i][2] or selection_list[2] == "") and (selection_list[3] == temp_data[i][3] or selection_list[3] == "") and (selection_list[4] == temp_data[i][4] or selection_list[4] == ""):
+                    print("{:<4}|{:<9}|{:<36}|{:<16}|{:<13}|{:<11}|{:<6}|".format(f"{i}.",temp_data[i][0],temp_data[i][1],temp_data[i][2],temp_data[i][3],temp_data[i][4],temp_data[i][5]))
+
+
+
