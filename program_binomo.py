958ec9803de5ea4eefecdaff15095a9cdc1cab85
Akmal Mahardika N P <89432383+4KMALM@users.noreply.github.com>
2022-04-24 16:35:31 +0700

    Update program_binomo.py

--- program_binomo.py
+++ program_binomo.py
@@ -1,7 +1,8 @@
-import imp
 from primitif_function import *
 from F02_register import register
 from F03_login import login
+from F04_tambah_game import tambah_game
+from F05_ubah_game import ubah_game
 from F06_ubah_stock import ubah_stock
 from F07_list_game_toko import list_game_toko
 from F08_buy_game import buy_game
@@ -12,12 +13,28 @@ from F15_load import load
 from F16_save import save
 from F17_exit import exit
 
+from B02_magic_shell import kerang_ajaib
+
 import os
+import sys
+import time
 
 path_folder = load()
 
-if path_folder != None:
-    print("Selamat datang ")
+if path_folder == None:
+    print()
+elif path_folder != None:
+    print("Mohon tunggu")
+    # sys.stdout.write("Loading")
+    # sys.stdout.flush()
+    # time.sleep(3)
+    # for i in range(3):
+    #     sys.stdout.write('.')
+    #     sys.stdout.flush()
+    #     time.sleep(1)
+
+    print("\nSelamat datang di antarmuka \"Binomo\"")
+
     # Pembuatan memori
     arrGame = buat_tabData(os.path.join(path_folder, 'game.csv'))
     arrUser = buat_tabData(os.path.join(path_folder, 'user.csv'))
@@ -36,22 +53,20 @@ if path_folder != None:
                     arrUser = register(arrUser)
 
                 elif user_input == "tambah_game":
-                    # tambah_game()
-                    # arrGame bertambah
+                    arrGame = tambah_game(arrGame)
                     pass
 
-                elif user_input == "ubah_stock":
+                elif user_input == "ubah_stok":
                     arrGame = ubah_stock(arrGame)
 
                 elif user_input == "topup":
                     arrUser = topup(arrUser)
+                
+                elif user_input == "ubah_game":
+                    arrGame = ubah_game(arrGame)
 
                 elif user_input == "buy_game":
                     arrGame, arrUser, arrKepemilikan, arrRiwayat = buy_game(pengguna[0], arrGame, arrUser, arrKepemilikan, arrRiwayat)
-                    # jika iduser belum punya game
-                    # arrRiwayat bertambah
-                    # arrKepemilikan bertambah
-                    pass
 
                 elif user_input == "list_game":
                     list_game(arrKepemilikan,arrGame,pengguna[0])
@@ -67,7 +82,7 @@ if path_folder != None:
                     pass
 
                 elif user_input == "list_game_toko":
-                    list_game_toko()
+                    list_game_toko(arrGame)
 
                 elif user_input == "search_game_at_store":
                     # search_game_at_store()
@@ -75,20 +90,24 @@ if path_folder != None:
                     pass
             
                 elif user_input == "save":
-                    save()
-                    # pakai semua array
-                    pass
+                    save(arrGame, arrUser, arrRiwayat, arrKepemilikan)
 
                 elif user_input == "exit":
-                    exit()
-                    pass
+                    exit(pengguna[2])
+                    exitGame = True
 
                 elif user_input == "help":
-                    help()
+                    help(isLogin,pengguna[4])
+                
+                elif user_input == "kerangajaib":
+                    kerang_ajaib()
 
         else:
             if user_input == "login":
-                login(pengguna,arrUser,isLogin)
-                isLogin = True
-            else:
+                pengguna = login(pengguna,arrUser)
+                if pengguna != ['id', 'username', 'nama', 'password', 'role', 'saldo']:
+                    isLogin = True
+            elif user_input == "help":
                 help(isLogin,pengguna[4])
+            else:
+                print("Masukan anda tidak dikenali.\nKetik \"help\" untuk panduan penggunaan BINOMO")
