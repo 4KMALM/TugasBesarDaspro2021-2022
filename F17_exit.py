031de397a77b7aa444f23152978b9f90d49e7bf7
Akmal Mahardika N P <89432383+4KMALM@users.noreply.github.com>
2022-04-24 16:47:51 +0700

    Update F17_exit.py

--- F17_exit.py
+++ F17_exit.py
@@ -1,14 +1,13 @@
 from F16_save import save
 
-def exit(nama):
+def exit(nama,arrGame, arrUser, arrRiwayat, arrKepemilikan):
     print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
     pil_exit = input()
     if pil_exit == "Y" or pil_exit == "y" :
-        save()
+        save(arrGame, arrUser, arrRiwayat, arrKepemilikan)
+        print(f"\nSampai jumpa {nama}")
     elif pil_exit == "N" or pil_exit == "n":
         print(f"Data terbaru tidak tersimpan dalam file.\n\nSampai jumpa {nama}")
     else : #jika input != Y, y, n, atau N
-        exit()
+        exit(nama,arrGame, arrUser, arrRiwayat, arrKepemilikan)
     return
-
-
